5150914b211e9fdc7c67b3a82c59effca15f34fe
Tia-11 <143324169+Tia-11@users.noreply.github.com>
2023-12-15 11:32:09 +0100

    Base + prime classi

---
+++ src/main/java/entities/Libri.java
@@ -0,0 +1,12 @@
+package entities;
+
+import java.util.UUID;
+
+public class Libri {
+    private UUID codiceISBN;
+    private String titolo;
+    private int annoPubblicazione;
+    private int numeroPagine;
+
+
+}
---
+++ src/main/java/entities/Riviste.java
@@ -0,0 +1,10 @@
+package entities;
+
+import java.util.UUID;
+
+public class Riviste {
+    private UUID codiceISBN;
+    private String titolo;
+    private int annoPubblicazione;
+    private int numeroPagine;
+}
